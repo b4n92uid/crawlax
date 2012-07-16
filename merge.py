4ac9c698e19d9988687dad088ce7e1afa8e2aae2
b4n92uid <b4n92uid@localhost>
2012-07-16 14:07:18 +0100

    Add id to sqlit db and improve the merge file

--- merge.py
+++ merge.py
@@ -3,19 +3,35 @@ import re
 import glob
 import sqlite3
 
-maindb = sqlite3.connect('database.sqlite')
+# INIT -------------------------------------------------------------------------
+
 firms = dict()
+
+mainfn = 'database.sqlite'
+
+if os.path.exists(mainfn):
+    os.remove(mainfn)
+
+maindb = sqlite3.connect(mainfn)
 
 cur = maindb.cursor()
 cur.execute('''
 CREATE TABLE IF NOT EXISTS firms(
+    id INTEGER NOT NULL PRIMARY KEY DEFAULT "0",
     name TEXT, address TEXT, category TEXT, phone TEXT, email TEXT, website TEXT, other TEXT
 )
 ''')
 maindb.commit()
 cur.close()
 
+# GLOB -------------------------------------------------------------------------
+
 for filename in glob.glob('*.sqlite'):
+
+    if filename == mainfn:
+        continue
+
+    print '>', filename
 
     database = sqlite3.connect(filename)
 
@@ -24,32 +40,39 @@ for filename in glob.glob('*.sqlite'):
 
     for entry in cur:
 
-        name = entry[0].lower().strip()
+        name = entry[1].lower().strip()
 
-        name = re.sub(u'[ÀÁÂÃÄÅàáâãäå]', 'a', name)
-        name = re.sub(u'[ÒÓÔÕÖØòóôõöø]', 'o', name)
-        name = re.sub(u'[ÈÉÊËèéêë]', 'e', name)
-        name = re.sub(u'[Çç]', 'c', name)
-        name = re.sub(u'[ÌÍÎÏìíîï]', 'i', name)
-        name = re.sub(u'[ÙÚÛÜùúûü]', 'u', name)
-        name = re.sub(u'[Ññ]', 'n', name)
-        name = re.sub(u'[ÿ]', 'y', name)
-        name = re.sub(u'[^a-zA-Z0-9]+', '-', name)
+        try:
+            name = re.sub(u'[ÀÁÂÃÄÅàáâãäå]', 'a', name)
+            name = re.sub(u'[ÒÓÔÕÖØòóôõöø]', 'o', name)
+            name = re.sub(u'[ÈÉÊËèéêë]', 'e', name)
+            name = re.sub(u'[Çç]', 'c', name)
+            name = re.sub(u'[ÌÍÎÏìíîï]', 'i', name)
+            name = re.sub(u'[ÙÚÛÜùúûü]', 'u', name)
+            name = re.sub(u'[Ññ]', 'n', name)
+            name = re.sub(u'[ÿ]', 'y', name)
+            name = re.sub(u'[^a-zA-Z0-9]+', '-', name)
+
+        except:
+            pass
+
+        if not name in firms.keys():
+            firms[name] = entry
 
         print name
 
-        #if not name in firms.keys():
-        #    firms[name] = entry
+# INSERT -----------------------------------------------------------------------
+
+mcur = maindb.cursor()
+
+for f in firms.values():
+
+    mcur.execute('''INSERT INTO firms(name, address, category, phone, email, website, other)
+                    VALUES(?, ?, ?, ?, ?, ?, ?)''',
+                    (f['name'], f['address'], f['category'], f['phone'], f['email'], f['website'], f['other']))
 
 
-#mcur = maindb.cursor()
-#
-#for f in firms.values():
-#
-#    mcur.execute('''INSERT INTO firms(name, address, category, phone, email, website, other)
-#                    VALUES(?, ?, ?, ?, ?, ?, ?)''',
-#                    (f['name'], f['address'], f['category'], f['phone'], f['email'], f['website'], f['other']))
-#
-#
-#maindb.commit()
-#mcur.close()
+maindb.commit()
+mcur.close()
+
+os.system('pause')
